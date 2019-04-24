f70c6ef8f9c4b0d33ab78836ad766b4722727023
miaomiao <1186586078@qq.com>
2019-04-24 22:11:36 +0800

    王文雨的修改

--- ManageStu/src/entity/Student.java
+++ ManageStu/src/entity/Student.java
@@ -1,32 +1,32 @@
-package entity;
+package entity;//database包
 
-public class Student {
-	private String name;
+public class Student {//student类
+	private String name; //student的相关属性
 	private String birDate;
 	private String gender;
 	private int id;
-	public String getName() {
+	public String getName() {//获取名字
 		return name;
 	}
-	public void setName(String name) {
+	public void setName(String name) {//创建名字
 		this.name = name;
 	}
-	public String getBirDate() {
+	public String getBirDate() {//获取生日
 		return birDate;
 	}
-	public void setBirDate(String birDate) {
+	public void setBirDate(String birDate) {//创建生日
 		this.birDate = birDate;
 	}
-	public String getGender() {
+	public String getGender() {//获取性别
 		return gender;
 	}
-	public void setGender(String gender) {
+	public void setGender(String gender) {//创造性别
 		this.gender = gender;
 	}
-	public int getId() {
+	public int getId() {//获取id
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(int id) {//创造id
 		this.id = id;
 	}
 }
