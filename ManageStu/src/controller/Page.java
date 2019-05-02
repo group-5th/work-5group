29ff883631e7a9489e1901877d271c2050c6ba97
miaomiao <1186586078@qq.com>
2019-05-02 12:00:11 +0800

    吴建瑜的修改

--- ManageStu/src/controller/Page.java
+++ ManageStu/src/controller/Page.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package controller;
 
 public class Page {
@@ -17,7 +18,31 @@ public class Page {
 		}
 		else{
 			currentPage=1;
-		}	
+		}
 		return currentPage;
 	}
 }
+=======
+package controller;
+
+public class Page {
+    //	计算当前页面数
+    public int countPage(String current, String total) {
+        int currentPage;//当前页
+        if (current != null && !current.equals("")) {
+            String current1 = current.trim();
+            currentPage = Integer.parseInt(current1);
+            int totalPage = Integer.parseInt(total);
+            if (currentPage > totalPage) {
+                currentPage = totalPage;
+            }
+            if (currentPage < 1) {
+                currentPage = 1;
+            }
+        } else {
+            currentPage = 1;
+        }
+        return currentPage;
+    }
+}
+>>>>>>> page页面审查
